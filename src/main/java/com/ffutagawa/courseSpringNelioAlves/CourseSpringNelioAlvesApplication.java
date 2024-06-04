06cf34aad1c781df1f0d89066d8c618b3e5ba5a5
FFutagawa <ffutagawa@gmail.com>
2024-06-03 21:07:10 -0300

    Project created

---
+++ src/main/java/com/ffutagawa/courseSpringNelioAlves/CourseSpringNelioAlvesApplication.java
@@ -0,0 +1,13 @@
+package com.ffutagawa.courseSpringNelioAlves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringNelioAlvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringNelioAlvesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ffutagawa/courseSpringNelioAlves/CourseSpringNelioAlvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffutagawa.courseSpringNelioAlves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringNelioAlvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
