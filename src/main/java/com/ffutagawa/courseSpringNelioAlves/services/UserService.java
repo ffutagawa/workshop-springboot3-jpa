967b9f2b065965a73fbc1e875c38665d3033fcbc
FFutagawa <ffutagawa@gmail.com>
2024-06-09 21:13:22 -0300

    Exception handling - findById

--- src/main/java/com/ffutagawa/courseSpringNelioAlves/services/UserService.java
+++ src/main/java/com/ffutagawa/courseSpringNelioAlves/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.ffutagawa.courseSpringNelioAlves.entities.User;
 import com.ffutagawa.courseSpringNelioAlves.repositories.UserRepository;
+import com.ffutagawa.courseSpringNelioAlves.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj= repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
